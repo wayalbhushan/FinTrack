ce160fa7d42c8d90313bfcb22a8b31577abf66f8
Bhushan Wayal <wayalbhushan7144@gmail.com>
2026-05-23 23:31:40 +0530

    refactor(test): remove unused import DataIntegrityViolationException in RepositoryTests

--- src/test/java/com/personalfinance/manager/repository/RepositoryTests.java
+++ src/test/java/com/personalfinance/manager/repository/RepositoryTests.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.dao.DataIntegrityViolationException;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
